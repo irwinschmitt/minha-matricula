85a6ead0a8114a53ac32509d53356c753950affa
Irwin Schmitt <irwinschmitt@gmail.com>
2020-12-07 20:10:04 -0300

    Add AppDrawer (main drawer)

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <v-app>
-    <AppToolbar @toggle-drawer="toggleDrawer" />
+    <AppToolbar
+      @toggle-drawer="$refs.appDrawer.drawer = !$refs.appDrawer.drawer"
+    />
+    <AppDrawer ref="appDrawer" />
     <router-view />
   </v-app>
 </template>
 
 <script>
 import AppToolbar from "@/components/AppToolbar.vue";
+import AppDrawer from "@/components/AppDrawer.vue";
 
 export default {
   name: "App",
   components: {
     AppToolbar,
-  },
-  methods: {
-    toggleDrawer() {
-      console.log("toggle drawer");
-    },
+    AppDrawer,
   },
 };
 </script>
---
+++ src/components/AppDrawer.vue
@@ -0,0 +1,15 @@
+<template>
+  <section id="app-drawer">
+    <v-navigation-drawer v-model="drawer" app right width="300">
+    </v-navigation-drawer>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AppDrawer",
+  data: () => ({
+    drawer: false,
+  }),
+};
+</script>
