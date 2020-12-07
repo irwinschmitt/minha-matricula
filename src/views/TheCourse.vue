2601c9133645d32005f605b1b5ecab1973588f34
Irwin Schmitt <irwinschmitt@gmail.com>
2020-12-07 18:40:47 -0300

    Set up plugins

---
+++ src/views/TheCourse.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <h1>Course</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TheCourse",
+};
+</script>
---
+++ src/views/TheSchedule.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <h1>Schedule</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TheSchedule",
+};
+</script>
