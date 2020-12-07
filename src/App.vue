31a59b26fdd81573c9b07527bdf37a1b30bc432c
Irwin Schmitt <irwinschmitt@gmail.com>
2020-12-07 19:58:53 -0300

    Add AppToolbar (main toolbar)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
   <v-app>
+    <AppToolbar @toggle-drawer="toggleDrawer" />
     <router-view />
   </v-app>
 </template>
+
+<script>
+import AppToolbar from "@/components/AppToolbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppToolbar,
+  },
+  methods: {
+    toggleDrawer() {
+      console.log("toggle drawer");
+    },
+  },
+};
+</script>
---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <section id="app-toolbar">
+    <v-app-bar app flat outlined>
+      <v-app-bar-nav-icon>
+        <v-img src="@/assets/logo.svg" width="50" to="/" />
+      </v-app-bar-nav-icon>
+
+      <v-spacer />
+
+      <v-tabs right :value="getTab()">
+        <v-tab to="/">Busca</v-tab>
+        <v-tab to="/grade">Sugestão</v-tab>
+      </v-tabs>
+
+      <v-btn icon @click="$emit('toggle-drawer')">
+        <v-icon>mdi-format-list-bulleted-square</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AppToolbar",
+  methods: {
+    getTab() {
+      switch (this.$route.name) {
+        case "Search":
+          return "tab-1";
+        case "Schedule":
+          return "tab-2";
+        case "Course":
+          return "tab-1";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.theme--light.v-app-bar.v-toolbar.v-sheet {
+  background-color: white;
+}
+</style>
