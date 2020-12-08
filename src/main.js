e891ec13ccaa78e257b65f7029c464ee58d5de26
Irwin Schmitt <irwinschmitt@gmail.com>
2020-12-08 02:00:46 -0300

    Add Vue InstantSearch

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import store from "./store";
+import InstantSearch from "vue-instantsearch";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(InstantSearch);
 
 new Vue({
   vuetify,
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
