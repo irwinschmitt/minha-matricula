c052ce1aecf3296d044106af47eeb279f61eb52a
Irwin Schmitt <irwinschmitt@gmail.com>
2021-01-17 13:04:26 -0300

    List courses in SearchList

---
+++ src/components/SearchList.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row>
+    <v-col v-for="course in 10" :key="course" cols="12" sm="6" md="4" xl="3">
+      <span>{{ course }}</span>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "SearchList",
+};
+</script>
--- src/views/TheSearch.vue
+++ src/views/TheSearch.vue
@@ -1,16 +1,19 @@
 <template>
   <section>
     <SearchToolbar />
+    <SearchList />
   </section>
 </template>
 
 <script>
 import SearchToolbar from "@/components/SearchToolbar";
+import SearchList from "@/components/SearchList";
 
 export default {
   name: "TheSearch",
   components: {
     SearchToolbar,
+    SearchList,
   },
 };
 </script>
